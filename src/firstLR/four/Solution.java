8e38365d63ca9255d0cdde0227d672c07a9edc8a
Roman Mesler <pay2mesler@gmail.com>
2024-02-22 09:41:06 +0500

    finish task3 lab1

---
+++ src/firstLR/four/Solution.java
@@ -0,0 +1,19 @@
+package firstLR.four;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] splitFirst = scanner.nextLine().split(" ");
+        int n = Integer.parseInt(splitFirst[0]);
+        int m = Integer.parseInt(splitFirst[1]);
+        int mod = Integer.parseInt(splitFirst[2]);
+
+        List<Integer> list = new ArrayList<>();
+
+
+    }
+}
