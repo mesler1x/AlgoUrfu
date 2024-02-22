2c992004ebf0d5a47b1db83b9869536625d35e59
Roman Mesler <pay2mesler@gmail.com>
2024-02-22 11:35:30 +0500

    finish task4 lab1

--- src/firstLR/four/Solution.java
+++ src/firstLR/four/Solution.java
@@ -8,12 +8,34 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] splitFirst = scanner.nextLine().split(" ");
-        int n = Integer.parseInt(splitFirst[0]);
+        int maxPower = Integer.parseInt(splitFirst[0]);
         int m = Integer.parseInt(splitFirst[1]);
         int mod = Integer.parseInt(splitFirst[2]);
 
-        List<Integer> list = new ArrayList<>();
+        long[] coefficients = new long[maxPower + 1];
+        for (int i = 0; i < maxPower + 1; i++) {
+            coefficients[i] = scanner.nextInt();
+        }
 
+        int[] arguments = new int[m];
+        for (int i = 0; i < m; i++) {
+            int x = scanner.nextInt();
+            arguments[i] = x;
+        }
 
+        for (int i = 0; i < m; i++) {
+            //long x = scanner.nextLong();
+            int res = calculate(coefficients, arguments[i], mod, maxPower);
+            System.out.println(res);
+        }
+    }
+
+    private static int calculate(long[] coefficients, long x, int mod, int maxPower) {
+        long result = 0;
+        for (int i = 0; i < coefficients.length; i++) {
+            result += (long) ((coefficients[i] * Math.pow(x, maxPower)));
+            maxPower--;
+        }
+        return (int) (result % mod); // вычисление MOD тоесть 4 mod 10 = 4
     }
 }
