3a46e5eebd45a34a46271a99c64d578cf8ed7f58
Roman Mesler <pay2mesler@gmail.com>
2024-03-19 08:27:49 +0500

    finish task 10

--- src/secondLR/ten/Solution.java
+++ src/secondLR/ten/Solution.java
@@ -22,35 +22,36 @@ public class Solution {
     }
 
     private static long findRes(int[] arr) {
-        List<Integer> t = new ArrayList<>();
+        List<Integer> people = new ArrayList<>();
         long ans = 0;
         Arrays.sort(arr);
         for (int i = 0; i < arr.length; i++) {
-            t.add(arr[i]);
+            people.add(arr[i]);
         }
-        while (t.size() >= 4) {
-            int _4 = t.get(t.size() - 1);
-            int _3 = t.get(t.size() - 2);
-            int _2 = t.get(1);
-            int _1 = t.get(0);
+        while (people.size() >= 4) {
+            int _4 = people.get(people.size() - 1);
+            int _3 = people.get(people.size() - 2);
+            int _2 = people.get(1);
+            int _1 = people.get(0);
 
             if (_4 + _1 + _3 + _1 < _2 + _1 + _4 + _2) {// сравниваем кто по максимальным выиграет
                 // такое сравнение потому что они обра вернутся за последним
-                ans += _4 + _1 + _3 + _1;// берем 2х максимальных и переводим их по одному
-                t.remove(t.size() - 1);// убираем только 2х максимальных потому что первый возвратился
-                t.remove(t.size() - 1);
+                ans += _4 + _1 + _3 + _1;// берем 2х максимальных и переводим их по одному - это быстрее
+                people.remove(people.size() - 1);// убираем только 2х максимальных потому что первый возвратился
+                people.remove(people.size() - 1);
             } else {
                 ans += _2 + _1 + _4 + _2;// отпарвляем 2х максимальных вдвоем
-                t.remove(t.size() - 1);// так же убираем 2х максимальных которые ушли
-                t.remove(t.size() - 1);// 2 в итоге вернулся что бы забрать 1
+                people.remove(people.size() - 1);// так же убираем 2х максимальных которые ушли
+                people.remove(people.size() - 1);// 2 в итоге вернулся что бы забрать 1
             }
         }
-        if (t.size() == 3) {
-            ans += t.get(0) + t.get(1) + t.get(2);
-        } else if (t.size() == 2) {
-            ans += t.get(1);
-        } else if (t.size() == 1) {
-            ans += t.get(0);
+        // в итоге сводя задачку постоянно к n-2 приходям к данным кейсам, уже очевидным
+        if (people.size() == 3) {
+            ans += people.get(0) + people.get(1) + people.get(2);
+        } else if (people.size() == 2) {
+            ans += people.get(1);
+        } else if (people.size() == 1) {
+            ans += people.get(0);
         }
         return ans;
     }
