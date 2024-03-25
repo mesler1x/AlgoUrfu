e3eb7d1232d9aea7dbc19a8fb5247bb0a638e18b
Roman Mesler <pay2mesler@gmail.com>
2024-03-25 13:56:26 +0500

    finish task 4

--- src/thirdLR/_4/Solution.java
+++ src/thirdLR/_4/Solution.java
@@ -1,7 +1,30 @@
 package thirdLR._4;
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("C:\\Users\\mesle\\Desktop\\DEV\\AlgoUrfu\\src\\thirdLR\\_4\\input.txt"));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        String[] operations = new String[n];
+        for (int i = 0; i < n; i++) {
+            operations[i] = bufferedReader.readLine();
+        }
+
+        PriorityQueue priorityQueue = new PriorityQueue();
+        for (int i = 0; i < operations.length; i++) {
+            if (operations[i].startsWith("A")) {
+                int val = Integer.parseInt(operations[i].substring(2));
+                priorityQueue.add(val);
+            } else if (operations[i].startsWith("X")) {
+                System.out.println(priorityQueue.pop());
+            } else {
+                String line = operations[i];
+                int index = Integer.parseInt(line.split(" ")[1]);
+                int element = Integer.parseInt(operations[index - 2].substring(2));
+                int elementToReplace = Integer.parseInt(line.split(" ")[2]);
+                priorityQueue.replace(element, elementToReplace);
+            }
+        }
     }
 }
