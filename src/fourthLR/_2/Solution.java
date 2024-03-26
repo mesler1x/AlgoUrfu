6c86b67fcfeecece36e87017457602269ce4d95d
Roman Mesler <pay2mesler@gmail.com>
2024-03-26 19:27:52 +0500

    start 4 and 3

---
+++ src/fourthLR/_2/Solution.java
@@ -0,0 +1,7 @@
+package fourthLR._2;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
--- src/thirdLR/_8/Solution.java
+++ src/thirdLR/_8/Solution.java
@@ -1,6 +1,18 @@
 package thirdLR._8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    List<List<Integer>> g = new ArrayList<>();
+    List<Integer> dist = new ArrayList<>();
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
 
     }
 }
