09bb10c7510c3a2bc9c1f4f001426b43f279c4a2
Roman Mesler <pay2mesler@gmail.com>
2024-04-01 23:40:21 +0500

    finish 4

--- src/_5LR/_4/Solution.java
+++ src/_5LR/_4/Solution.java
@@ -3,40 +3,41 @@ package _5LR._4;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
+import java.math.BigInteger;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader("/home/mesler/Рабочий стол/AlgoUrfu/src/_5LR/_4/input.txt"));
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("C:\\Users\\mesle\\Desktop\\DEV\\AlgoUrfu\\src\\_5LR\\_4\\input.txt"));
         int n = Integer.parseInt(bufferedReader.readLine());
-        long[] array = new long[n];
+        BigInteger[] array = new BigInteger[n];
         for (int i = 0; i < array.length; i++) {
-            array[i] = Integer.parseInt(bufferedReader.readLine());
+            array[i] = new BigInteger(bufferedReader.readLine());
         }
 
         for (int i = 0; i < array.length; i++) {
-            if (isFullSquared(array[i])) {
+            if (isSqrt(array[i])) {
                 System.out.println(i + 1);
             }
         }
     }
 
-    private static boolean isFullSquared(long number) {
-        Map<Long, Integer> map = new HashMap<>();
-        for (int i = 2; i <= number ; i++) {
-            while (number % i == 0) {
-                number = number / i;
-                map.put(number, map.getOrDefault(number, 0) + 1);
+    public static boolean isSqrt(BigInteger n) {
+        BigInteger low = BigInteger.ZERO;
+        BigInteger high = n.add(BigInteger.ONE);
+        while (low.compareTo(high.subtract(BigInteger.ONE)) < 0) { // пока low < high - 1
+            BigInteger mid = low.add(high).divide(BigInteger.valueOf(2)); // (low + high) / 2
+            if (n.compareTo(mid.multiply(mid)) < 0) { // if (mid * mid > n)
+                high = mid;
+            } else if (n.compareTo(mid.multiply(mid)) == 0) {
+                return true; // если нашли число выходим
+            } else {
+                low = mid; // if (mid * mid) < n
             }
         }
 
-        for(Map.Entry<Long, Integer> entry : map.entrySet()) {
-            if (entry.getValue() % 2 != 0) {
-                return false;
-            }
+        if (low.multiply(low).equals(n)) {
+            return true;
         }
-        return true;
+        return false;
     }
 }
