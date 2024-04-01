9115df79601f617d54edd1a3d35765259d1cff82
Roman Mesler <meslerbeats@gmail.com>
2024-04-01 22:38:35 +0500

    task 4

--- src/_5LR/_3/Solution.java
+++ src/_5LR/_3/Solution.java
@@ -1,6 +1,51 @@
 package _5LR._3;
 
 
-public class Solution {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("/home/mesler/Рабочий стол/AlgoUrfu/src/_5LR/_3/input.txt"));
+        String word = bufferedReader.readLine();
+        String pattern = bufferedReader.readLine();
+        String answer = match(word, pattern);
+        System.out.println(answer);
+    }
+
+    public static String match(String fileName, String pattern) {
+        int i = 0;
+        int j = 0;
+        int starIndex = -1;
+        int iIndex = -1;
+
+        while (i < fileName.length()) {
+            if (j < pattern.length() && (pattern.charAt(j) == '?' || fileName.charAt(i) == pattern.charAt(j))) {
+                // если символы совпадают либо паттерн содержит любой символ (?)
+                i++;
+                j++;
+            } else if (j < pattern.length() && pattern.charAt(j) == '*') {
+                //если встречаем * запоминаем индекс
+                starIndex = j;// позиция ПОСЛЕДНЕЙ звезды
+                iIndex = i;// позиция имени файла c которого встретилась звезда
+                j++;
+            } else if (starIndex != -1) {
+                j = starIndex + 1;// перекидываем за звезду
+                // индекс j все равно не двинется на 1 больше чем последняя звезда
+                i = iIndex + 1;//перекидываем символ в файле
+                iIndex++;
+            } else { // если символы не совпадают и нет * и нет ?
+                return "NO";
+            }
+        }
+
+        //если есть оставшиеся звезды
+        while (j < pattern.length() && pattern.charAt(j) == '*') {
+            j++;
+        }
+
+        return j == pattern.length() ? "YES" : "NO";
+    }
 }
+
--- src/_5LR/_4/Solution.java
+++ src/_5LR/_4/Solution.java
@@ -1,4 +1,42 @@
 package _5LR._4;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("/home/mesler/Рабочий стол/AlgoUrfu/src/_5LR/_4/input.txt"));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        long[] array = new long[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(bufferedReader.readLine());
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (isFullSquared(array[i])) {
+                System.out.println(i + 1);
+            }
+        }
+    }
+
+    private static boolean isFullSquared(long number) {
+        Map<Long, Integer> map = new HashMap<>();
+        for (int i = 2; i <= number ; i++) {
+            while (number % i == 0) {
+                number = number / i;
+                map.put(number, map.getOrDefault(number, 0) + 1);
+            }
+        }
+
+        for(Map.Entry<Long, Integer> entry : map.entrySet()) {
+            if (entry.getValue() % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
